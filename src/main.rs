b8d71816958c578e9ef34afc0317bdaada72dcdf
lkbhargav <lkbhargav9@gmail.com>
2023-03-28 23:43:57 -0400

    Updated the dir name

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ async fn main() {
             println!("detection happened, sensor: {detection_name}, time: {datetime:?} ");
 
             let datetime = datetime.replace(" ", "");
+            let datetime = datetime.replace(":", "");
             let prefix = format!("/var/log/images/{datetime}");
             println!("Directory name: {prefix}");
             fs::create_dir(prefix.clone()).expect("trying to create a directory");
